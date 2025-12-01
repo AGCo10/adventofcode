d59477575f73bf46209dc48bd58ba8379f0b2ec9
Adam Gaines <adam.j.gaines.mil@swf.army.mil>
2025-12-01 13:41:57 -0600

    Solution Added

--- Day_1/src/main/java/swf/army/mil/Main.java
+++ Day_1/src/main/java/swf/army/mil/Main.java
@@ -1,10 +1,21 @@
 package swf.army.mil;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.io.File;
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        String[] Turns = {"L68", "L30", "R48", "L5", "R60" , "L55" , "L1", "L99", "R14", "L82"};
+    public static void main(String[] args) throws IOException {
+
+        File myFile = new File("/Users/a2473390/Desktop/adventocode/adventofcode/Day_1/src/main/resources/Dial_Turns");
+
+
+
+        String[] Turns = Files.readAllLines(myFile.toPath()).toArray(new String[0]);
+
         Character[] Directions = new Character[10];
         Integer[] Ticks = new Integer[10];
         for (int index = 0; index <= Turns.length-1; index++) {
@@ -16,6 +27,37 @@ public class Main {
         for (int index = 0; index <= Turns.length-1; index++) {
             System.out.println("Direction: " + Directions[index] + " Ticks: " + Ticks[index]);
         }
+
+        int Dial = 50;
+        int ZeroCount = 0;
+
+        for(int index = 0; index <= Turns.length-1; index++) {
+            if (Directions[index] == 'L') {
+                if (Dial - Ticks[index] < 0) {
+                    Dial = 100 - (Ticks[index]-Dial);
+                }
+                else {
+                    Dial -= Ticks[index];
+                }
+                if (Dial == 0) {
+                    ZeroCount++;
+                }
+            }
+            else {
+                if (Dial + Ticks[index] > 99) {
+                    Dial =(Ticks[index]+Dial) - 100;
+                }
+                else {
+                    Dial += Ticks[index];
+                }
+                if (Dial == 0) {
+                    ZeroCount++;
+                }
+            }
+            System.out.println("Dial is: " + Dial);
+        }
+        System.out.println("ZeroCounter: " + ZeroCount);
+
 
     }
 }
